6f30fc19758b117caa7519e52f1d44e827449187
Peter Mueller <peter.mueller@uni-wuerzburg.de>
2026-01-12 23:29:29 +0100

    doof

--- fuer_volker/create_gall_2.py
+++ fuer_volker/create_gall_2.py
@@ -3,13 +3,13 @@ file = open('gall2.html', 'w')
 for zae in range(60):
 #for zae in range(10):
     zs = str(zae).rjust(2, "0")
-    s = f'''<h3>Fall {zs}</h3>
-    <a href="Data/teile_{zs}.html" class="gallery-item">
+    s = f'''<a href="Data/teile_{zs}.html" class="gallery-item">
     <img src="Data/teile_{zs}.png" alt="Image {zs}">
+    <p>Fall {zs}</p>
     </a>
     <a href="Data/tet_{zs}.html" class="gallery-item">
     <img src="Data/tet_{zs}.png" alt="Image {zs}">
-    </a><br>'''    
+    </a>'''    
     file.write(s + '\n')
 file.close()
 
