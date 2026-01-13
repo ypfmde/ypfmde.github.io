b9d177d3fc7d1cc8243b9a80b4c6094659993ffd
Peter Mueller <peter.mueller@uni-wuerzburg.de>
2026-01-13 11:42:01 +0100

    doof

--- fuer_volker/create_gall_3.py
+++ fuer_volker/create_gall_3.py
@@ -4,7 +4,7 @@ for zae in range(396):
 #for zae in range(10):
     zs = str(zae).rjust(3, "0")
     s = f'''        <a href="Data/fall_58_{zs}.html" class="gallery-item">
-            <img src="Data/fall_58_{zs}.png" alt="Image {zs}">
+            <img src="Data/fall_58_{zs}.png" loading="lazy" alt="Image {zs}">
             <p>Fall {zs}</p>
         </a>'''
     file.write(s + '\n')
